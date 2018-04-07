a933439d4266e6d97583336863741e365ff5a4ae
Shvonne_Z <shiyuanzhang1997@163.com>
2018-04-07 20:57:28 +0800

    Add files via upload

---
+++ .net/Experiment/1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入用户名：");
+            string welcomeString = Console.ReadLine();
+            Console.WriteLine("Welcome {0} ！", welcomeString);
+            Console.ReadLine();
+        }
+    }
+}
