1ae94dfadb7c379fbb6945f89ce6c8a931319ca9
Shvonne_Z <shiyuanzhang1997@163.com>
2018-05-21 02:09:30 +0800

    Add files via upload

---
+++ .net/Experiment/5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Experiment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack pail = new Stack(10);
+            /*Thread pro1 = new Thread(pail.Push);
+            pro1.Name = "师傅1";
+            pro1.Start("葱油饼");*/
+            for (int i = 0; i < 20; i++)
+            {
+                Thread th = new Thread(pail.Push);
+                th.Start("葱油饼");
+                Thread th1 = new Thread(pail.Pop);
+                th1.Start();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ .net/Experiment/5/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment5
+{
+    class Stack
+    {
+        private int size = 10;
+        private object[] StackSpace;
+        private int top;
+
+        public Stack(int size)
+        {
+            this.size = size;
+            StackSpace = new object[size];
+            top = -1;
+        }
+        public void Push(object c)
+        {
+            lock (this)
+            {
+                if (top == StackSpace.Length - 1)
+                {
+                    Console.WriteLine("桶已满！");
+                }
+                else
+                {
+                    StackSpace[++top] = c;
+                    Console.WriteLine("葱油饼+1,共{0}个葱油饼",top+1);
+                }
+            }
+            
+        }
+        public void Pop()
+        {
+            lock (this)
+            {
+                if (top == -1)
+                {
+                    Console.WriteLine("桶已空！");
+                   // return Convert.ToChar(0);
+                }
+                else
+                {
+                    Console.WriteLine("葱油饼-1,共{0}个葱油饼", top);
+                    top--;
+                }
+            }
+        }
+
+    }
+}
